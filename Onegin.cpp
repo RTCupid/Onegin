7721ccb514bcc56f755b9f990cc7a3fd458e0d2e
RTCupid <19apr2006@vk.com>
2024-09-10 17:54:54 +0300

    + Onegin.h vse rabotaet

--- Onegin.cpp
+++ Onegin.cpp
@@ -1,40 +1,4 @@
-#include "TXLib.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-
-const int MAX_ROWS = 29;
-const int MAX_ELEM = 100;
-
-// I have a commit!
-
-_Bool Input (char text[MAX_ROWS][MAX_ELEM]);
-
-void OutputText (char text[MAX_ROWS][MAX_ELEM]);
-
-void Sorting (char text[MAX_ROWS][MAX_ELEM]);
-
-_Bool Swapping (char* sFirst, char* sSecond);
-
-_Bool SkipMarks (char* sFirst, char* sSecond, int* iIndexElem, int* jIndexElem);
-
-//{
-/// @brief   Comparator - функция, которая сравнивает две строки
-///                       и выводит разницу между первыми не равными
-///                       элементами строки. То есть показывает какая
-///                       из строк должна стоять раньше
-/// @param   sFirst     - первая из сравниваемых строк
-/// @param   sSecond    - вторая из сравниваемых строк
-///
-/// @return  разницу между первыми не равными символами между строками
-///          разница отрицательна, если строка должна стоять выше,
-///          и положительна, если ниже
-/// @code
-///          Comparator (sFirst, sSecond);
-/// @endcode
-//}
-
-int Comparator (char* sFirst, char* sSecond);
+#include "Onegin.h"
 
 int main ()
     {
