6218d74fc0a6968856682c348281103ad74d39b7
RTCupid <19apr2006@vk.com>
2024-09-13 20:05:58 +0300

    schitivanie s fayla cherez fread

--- fread.cpp
+++ fread.cpp
@@ -3,8 +3,11 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <ctype.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
 
 _Bool InputMP ();
 
@@ -22,29 +25,58 @@ _Bool InputMP ()
     {
     struct stat fileInf;
 
+    stat ("Onegin.txt", &fileInf);
 
-    printf ("%lld\n", fileInf.st_size);
+    printf ("\n%lld\n", fileInf.st_size);
     printf ("count of char = %lld\n", fileInf.st_size / sizeof (char));
 
-    void* buffer = calloc (fileInf.st_size + 1, sizeof(char));
+    void* Onegin = calloc (fileInf.st_size + 1, sizeof(char));
 
-    FILE* file = fopen ("Onegin.txt", "rb");
+    FILE* file = fopen ("Onegin.txt", "rt");
 
     if (file == NULL)
         {
         printf ("File opening error\n");
-
+        printf("errno = <%d>\n", errno);
+        perror("Onegin.txt\n");
         return 0;
         }
 
-    if (feof (file))
-        clearerr (file);
+    size_t sizeOfFile = fread (Onegin, sizeof (char), fileInf.st_size, file);
 
-    size_t razmerFayla = fread (buffer, fileInf.st_size, sizeof (char), file);
+    if (sizeOfFile == 0)
+        {
+        printf ("errno = <%d>\n", errno);
+        perror ("Onegin.txt");
+        }
 
-    printf ("%s\n", buffer);
+    //for(int i=0; i<sys_nerr; i++)
+    //    printf("sys_errlist[%d] = \"%s\"\n", i, sys_errlist[i]);
+
+    printf ("\n%s\n", Onegin);
 
     fclose (file);
 
-    printf ("%zu\n", razmerFayla);
+    printf ("sizeOfFile = <%zu>\n\n", sizeOfFile);
+
+    int nRow = 0;
+
+    for (int i = 0; i < sizeOfFile; i++)
+        {
+        if (*((char*)Onegin + i * sizeof (char)) == '\n')
+            {
+            printf ("Onegin[%d] = <'\\n'>\n", i);
+            nRow = nRow + 1;
+            }
+        else
+            printf ("Onegin[%d] = <%c>\n", i, *((char*)Onegin + i * sizeof (char)));
+        }
+
+    printf ("nRow = <%d>", nRow);
+
+
+
+
+
+
     }
