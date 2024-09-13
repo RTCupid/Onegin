574fe93fad0054cadb891b10d1563e0906725234
RTCupid <19apr2006@vk.com>
2024-09-13 21:19:52 +0300

    Massiv ukazateley est, no ya ne znayu kak ego vivesty

--- fread.cpp
+++ fread.cpp
@@ -72,11 +72,35 @@ _Bool InputMP ()
             printf ("Onegin[%d] = <%c>\n", i, *((char*)Onegin + i * sizeof (char)));
         }
 
-    printf ("nRow = <%d>", nRow);
+    printf ("nRow = <%d>\n", nRow);                                    // I have this commit!
+
+    void* Pointers = calloc (nRow, sizeof (char*));
+
+    int nPointer = 0;
+
+    printf ("\nInitialization of Pointers:\n\n");
+
+    for (int i = 0; i < sizeOfFile; i++)
+        {
+        if (*((char*)Onegin + i * sizeof (char)) == '\n')
+            {
+            *((int*)Pointers + i * sizeof (int) + 1 * sizeof (int)) = (int)Onegin + i * sizeof (char);
+            printf ("Pointer[%d] = <%p>\n", nPointer, (int)Onegin + i * sizeof (char));
+            nPointer = nPointer + 1;
+            }
+        }
+
+    printf ("\nPointers:\n\n");
+
+    for (int n = 0; n < nPointer; n++)
+        {
+        printf ("Pointers[%d] = <%p>\n", n, *((int*)Pointers + n * sizeof (int)));
+        }
 
 
 
 
 
 
+    return 1;
     }
